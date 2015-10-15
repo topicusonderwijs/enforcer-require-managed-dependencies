815aee3fd9f5dd68c5a509004ef3e3fd5f91b51e
Emond Papegaaij <emond.papegaaij@topicus.nl>
2015-10-15 10:46:42 +0200

    support excludes

--- src/main/java/nl/topicus/onderwijs/maven/RequireManagedDependencies.java
+++ src/main/java/nl/topicus/onderwijs/maven/RequireManagedDependencies.java
@@ -1,5 +1,7 @@
 package nl.topicus.onderwijs.maven;
 
+import java.util.List;
+
 import org.apache.maven.enforcer.rule.api.EnforcerRule;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
@@ -10,59 +12,85 @@ import org.apache.maven.shared.dependency.graph.DependencyNode;
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 
-public class RequireManagedDependencies implements EnforcerRule {
-	private DependencyNode getNode(EnforcerRuleHelper helper)
-			throws EnforcerRuleException {
-		try {
+public class RequireManagedDependencies implements EnforcerRule
+{
+	private List<String> excludes;
+
+	private DependencyNode getNode(EnforcerRuleHelper helper) throws EnforcerRuleException
+	{
+		try
+		{
 			MavenProject project = (MavenProject) helper.evaluate("${project}");
-			DependencyGraphBuilder dependencyTreeBuilder = (DependencyGraphBuilder) helper
-					.getComponent(DependencyGraphBuilder.class);
-			DependencyNode node = dependencyTreeBuilder.buildDependencyGraph(
-					project, null);
+			DependencyGraphBuilder dependencyTreeBuilder =
+				(DependencyGraphBuilder) helper.getComponent(DependencyGraphBuilder.class);
+			DependencyNode node = dependencyTreeBuilder.buildDependencyGraph(project, null);
 			return node;
-		} catch (ExpressionEvaluationException e) {
+		}
+		catch (ExpressionEvaluationException e)
+		{
 			throw new EnforcerRuleException("Unable to lookup an expression "
-					+ e.getLocalizedMessage(), e);
-		} catch (ComponentLookupException e) {
+				+ e.getLocalizedMessage(), e);
+		}
+		catch (ComponentLookupException e)
+		{
 			throw new EnforcerRuleException("Unable to lookup a component "
-					+ e.getLocalizedMessage(), e);
-		} catch (DependencyGraphBuilderException e) {
+				+ e.getLocalizedMessage(), e);
+		}
+		catch (DependencyGraphBuilderException e)
+		{
 			throw new EnforcerRuleException("Could not build dependency graph "
-					+ e.getLocalizedMessage(), e);
+				+ e.getLocalizedMessage(), e);
 		}
 	}
 
-	public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException {
-		try {
+	@Override
+	public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException
+	{
+		try
+		{
+			helper.getLog().debug("RequireManagedDependencies excluding " + excludes);
 			DependencyNode node = getNode(helper);
-			UnmanagedDependencyCollector visitor = new UnmanagedDependencyCollector(
-					(MavenProject) helper.evaluate("${project}"), node);
-			node.accept(visitor);
+			UnmanagedDependencyCollector visitor =
+				new UnmanagedDependencyCollector((MavenProject) helper.evaluate("${project}"),
+					node, excludes);
+			if (!node.accept(visitor))
+			{
+				throw visitor.getStoredException();
+			}
+
 			boolean fail = false;
-			for (CharSequence unmanaged : visitor.getUnmanagedDependencies()) {
+			for (CharSequence unmanaged : visitor.getUnmanagedDependencies())
+			{
 				fail = true;
-				helper.getLog()
-						.warn("Unmanaged dependency found: " + unmanaged);
+				helper.getLog().warn("Unmanaged dependency found: " + unmanaged);
 			}
-			if (fail) {
-				throw new EnforcerRuleException(
-						"Failed while enforcing releasability. "
-								+ "See above detailed error message.");
+			if (fail)
+			{
+				throw new EnforcerRuleException("Failed while enforcing releasability. "
+					+ "See above detailed error message.");
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			throw new EnforcerRuleException(e.getLocalizedMessage(), e);
 		}
 	}
 
-	public String getCacheId() {
+	@Override
+	public String getCacheId()
+	{
 		return "";
 	}
 
-	public boolean isCacheable() {
+	@Override
+	public boolean isCacheable()
+	{
 		return false;
 	}
 
-	public boolean isResultValid(EnforcerRule rule) {
+	@Override
+	public boolean isResultValid(EnforcerRule rule)
+	{
 		return false;
 	}
 }
--- src/main/java/nl/topicus/onderwijs/maven/UnmanagedDependencyCollector.java
+++ src/main/java/nl/topicus/onderwijs/maven/UnmanagedDependencyCollector.java
@@ -1,49 +1,100 @@
 package nl.topicus.onderwijs.maven;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.apache.maven.model.Dependency;
+import org.apache.maven.plugins.enforcer.utils.ArtifactMatcher;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.dependency.graph.DependencyNode;
 import org.apache.maven.shared.dependency.graph.traversal.DependencyNodeVisitor;
 
-public class UnmanagedDependencyCollector implements DependencyNodeVisitor {
+public class UnmanagedDependencyCollector implements DependencyNodeVisitor
+{
 	private Set<String> managedDependencies;
+
 	private Set<String> unmanagedDependencies = new TreeSet<>();
+
 	private DependencyNode root;
 
-	public UnmanagedDependencyCollector(MavenProject project,
-			DependencyNode root) {
+	private List<String> excludes;
+
+	private EnforcerRuleException storedException;
+
+	public UnmanagedDependencyCollector(MavenProject project, DependencyNode root,
+			List<String> excludes)
+	{
 		if (project.getDependencyManagement() == null)
 			managedDependencies = Collections.emptySet();
 		else
-			managedDependencies = project.getDependencyManagement()
-					.getDependencies().stream()
-					.map(Dependency::getManagementKey)
-					.collect(Collectors.toSet());
+			managedDependencies =
+				project.getDependencyManagement().getDependencies().stream()
+					.map(Dependency::getManagementKey).collect(Collectors.toSet());
 		this.root = root;
+		this.excludes = excludes;
 	}
 
 	@Override
-	public boolean visit(DependencyNode node) {
+	public boolean visit(DependencyNode node)
+	{
 		if (node.equals(root))
 			return true;
 
-		if (!managedDependencies.contains(node.getArtifact()
-				.getDependencyConflictId()))
+		try
+		{
+			if (matchesExclude(node))
+				return true;
+		}
+		catch (InvalidVersionSpecificationException e)
+		{
+			storedException = new EnforcerRuleException("Invalid Version Range: ", e);
+			return false;
+		}
+
+		if (!managedDependencies.contains(node.getArtifact().getDependencyConflictId()))
 			unmanagedDependencies.add(node.toNodeString());
 		return true;
 	}
 
+	private boolean matchesExclude(DependencyNode node) throws InvalidVersionSpecificationException
+	{
+		for (String curExclude : excludes)
+		{
+			ArtifactMatcher.Pattern am = new ArtifactMatcher.Pattern(curExclude);
+			if (am.match(node.getArtifact()))
+				return true;
+		}
+		return false;
+	}
+
 	@Override
-	public boolean endVisit(DependencyNode node) {
+	public boolean endVisit(DependencyNode node)
+	{
 		return true;
 	}
 
-	public Set<String> getUnmanagedDependencies() {
+	public Set<String> getUnmanagedDependencies()
+	{
 		return unmanagedDependencies;
+	}
+
+	public EnforcerRuleException getStoredException()
+	{
+		return storedException;
+	}
+
+	public List<String> getExcludes()
+	{
+		return excludes;
+	}
+
+	public void setExcludes(List<String> excludes)
+	{
+		this.excludes = excludes;
 	}
 }
